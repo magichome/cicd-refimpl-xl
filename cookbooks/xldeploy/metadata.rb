1d1a6a76d1ed9121187748e0c334c5f067862c39
tfleming <tfleming@onebox.com>
2016-04-23 18:03:47 -0400

    initial checkin

---
+++ cookbooks/xldeploy/metadata.rb
@@ -0,0 +1,16 @@
+name    'xldeploy'
+version '1.0.1'
+
+description 'Installs and configures XL Deploy from XebiaLabs'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+supports 'centos'
+
+license 'Apache v2.0'
+
+source_url 'https://github.com/electric-it/xldeploy'
+issues_url 'https://github.com/electric-it/xldeploy/issues'
+
+depends 'runit',  '~> 1.5'
+depends 'java',   '~> 1.25'
+depends 'tar',    '~> 0.5'
---
+++ cookbooks/xltestview/metadata.rb
@@ -0,0 +1,16 @@
+name    'xlrelease'
+version '1.0.3'
+
+description 'Installs and configures XL Release from XebiaLabs'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+supports 'centos'
+
+license 'Apache v2.0'
+
+source_url 'https://github.com/electric-it/xlrelease'
+issues_url 'https://github.com/electric-it/xlrelease/issues'
+
+depends 'runit',  '~> 1.5'
+depends 'java',   '~> 1.25'
+depends 'tar',    '~> 0.5'
